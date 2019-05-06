e6217d92ed7d308bd9d77c3738ac1a5c6fd50b60
milan.zivanov <milan.zivanov@gmail.com>
2019-05-06 19:36:34 +0200

    back to top new one and scroll bar custom

--- js/main.js
+++ js/main.js
@@ -140,28 +140,24 @@ $(window).scroll(function() {
     }
 });
 
-// btt 
-if ($('.back-to-top').length) {
-    var scrollTrigger = 100, // px
-        backToTop = function () {
-            var scrollTop = $(window).scrollTop();
-            if (scrollTop > scrollTrigger) {
-                $('.back-to-top').addClass('show');
-            } else {
-                $('.back-to-top').removeClass('show');
-            }
-        };
-    backToTop();
-    $(window).on('scroll', function () {
-        backToTop();
-    });
-    $('.back-to-top').on('click', function (e) {
-        e.preventDefault();
-        $('html,body').animate({
-            scrollTop: 0
-        }, 500);
-    });
-}
+// btt
+jQuery(document).ready(function() {
+  
+  var btn = $('.back-to-top');
 
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 100) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+
+});
 
 
